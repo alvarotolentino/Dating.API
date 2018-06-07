7299db8692931c2d3d1d301fca3b4ba985d8e016
Alvaro Tolentino <alvarotolentinou@gmail.com>
2018-06-07 08:38:54 -0500

    Add appsetings

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,8 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var key = Encoding.ASCII.GetBytes("super secure key");
+            
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddScoped<IConnectionFactory, ConnectionFactory>();

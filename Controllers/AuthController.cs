ae00856c4aac32797a22a05875de02417ebca60b
Alvaro Junior TOLENTINO URBINA <alvaro.tolentino@ibm.com>
2018-06-25 18:33:42 -0500

    Add Error Handling

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -11,68 +11,64 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
-namespace Dating.API.Controllers
-{
-    [Route("api/[controller]")]
-    public class AuthController : Controller
-    {
+namespace Dating.API.Controllers {
+    [Route ("api/[controller]")]
+    public class AuthController : Controller {
 
         private readonly IAuthRepository _repository;
         private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository repository, IConfiguration configuration)
-        {
+        public AuthController (IAuthRepository repository, IConfiguration configuration) {
             _repository = repository;
             _configuration = configuration;
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
-        {
-            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-            if (await _repository.UserExists(userForRegisterDto.Username))
-            {
-                ModelState.AddModelError("Username", "Username already exists");
+        [HttpPost ("register")]
+        public async Task<IActionResult> Register ([FromBody] UserForRegisterDto userForRegisterDto) {
+            if (!string.IsNullOrWhiteSpace (userForRegisterDto.Username)) {
+                userForRegisterDto.Username = userForRegisterDto.Username.ToLower ();
+
+            }
+
+            if (await _repository.UserExists (userForRegisterDto.Username)) {
+                ModelState.AddModelError ("Username", "Username already exists");
             }
 
             if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+                return BadRequest (ModelState);
 
-            var userToCreate = new User
-            {
+            var userToCreate = new User {
                 UserName = userForRegisterDto.Username
             };
 
-            var createUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
+            var createUser = await _repository.Register (userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
+            return StatusCode (201);
         }
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
-        {
-            var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
-            if (userFromRepo == null)
-            {
-                return Unauthorized();
+        [HttpPost ("login")]
+        public async Task<IActionResult> Login ([FromBody] UserForLoginDto userForLoginDto) {
+            throw new Exception ("Computer says no!");
+
+            var userFromRepo = await _repository.Login (userForLoginDto.Username.ToLower (), userForLoginDto.Password);
+            if (userFromRepo == null) {
+                return Unauthorized ();
             }
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
-                    new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            var tokenHandler = new JwtSecurityTokenHandler ();
+            var key = Encoding.ASCII.GetBytes (_configuration.GetSection ("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity (new Claim[] {
+                new Claim (ClaimTypes.NameIdentifier, userFromRepo.Id.ToString ()),
+                new Claim (ClaimTypes.Name, userFromRepo.UserName)
                 }),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                Expires = DateTime.Now.AddDays (1),
+                SigningCredentials = new SigningCredentials (new SymmetricSecurityKey (key),
                 SecurityAlgorithms.HmacSha512Signature)
             };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-            return Ok(new { tokenString });
+            var token = tokenHandler.CreateToken (tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken (token);
+            return Ok (new { tokenString });
         }
 
     }
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Dating.API.Helpers;
 using Dating.API.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -15,56 +19,55 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
-namespace DatingApp.API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace DatingApp.API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddCors();
-            services.AddScoped<IConnectionFactory, ConnectionFactory>();
-            services.AddScoped<IDataRepository, DataRepository>();
-            services.AddScoped<IAuthRepository, AuthRepository>();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
+        public void ConfigureServices (IServiceCollection services) {
+
+            var key = Encoding.ASCII.GetBytes (Configuration.GetSection ("AppSettings:Token").Value);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+            services.AddCors ();
+            services.AddScoped<IConnectionFactory, ConnectionFactory> ();
+            services.AddScoped<IDataRepository, DataRepository> ();
+            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer (options => {
+                    options.TokenValidationParameters = new TokenValidationParameters {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    IssuerSigningKey = new SymmetricSecurityKey (key),
                     ValidateIssuer = false,
                     ValidateAudience = false
-                };
-            });
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            // if (env.IsDevelopment())
-            // {
-            app.UseDeveloperExceptionPage();
-            // }
-            // else
-            // {
-            //     app.UseHsts();
-            // }
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler (builder => {
+                    builder.Run (async context => {
+                        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                        var error = context.Features.Get<IExceptionHandlerFeature> ();
+                        if (error != null) {
+                            context.Response.AddApplicationAError(error.Error.Message);
+                            await context.Response.WriteAsync (error.Error.Message);
+                        }
+                    });
+                });
+            }
 
             // app.UseHttpsRedirection();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
-            app.UseAuthentication();
-            app.UseMvc();
+            app.UseCors (x => x.AllowAnyHeader ().AllowAnyMethod ().AllowAnyOrigin ().AllowCredentials ());
+            app.UseAuthentication ();
+            app.UseMvc ();
         }
     }
 }
